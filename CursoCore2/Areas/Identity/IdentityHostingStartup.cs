35c650b09c18e8bc81c8b2ed4a63a33a74394714
felixbvf <felixbvf@gmail.com>
2019-07-17 10:00:02 -0400

    Crear app, empaquetado login, Create DB, migracion, reister user, create service roles and asing role to user

---
+++ CursoCore2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CursoCore2.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CursoCore2.Areas.Identity.IdentityHostingStartup))]
+namespace CursoCore2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ CursoCore2/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CursoCore2.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace CursoCore2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IServiceProvider serviceProvider)
+        {
+           CreateRoles(serviceProvider).Wait();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            //Creando roles
+            String[] rolesName = { "Admin", "User" };
+            IdentityResult roleResult;
+            foreach (var item in rolesName)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(item);
+                if(!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(item));
+                }
+            }
+            //Asignando Rol a un usuario
+            var user = await userManager.FindByIdAsync("f38e402c-bfcd-4786-bdac-8448a10ae6e9");
+            await userManager.AddToRoleAsync(user, "Admin");
+        }
+    }
+}
